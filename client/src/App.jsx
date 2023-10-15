49320bbbafbd4f5b4b7c8f0900772d9f393c644a
mhp <mhperera1995@gmail.com>
2023-10-15 13:14:18 +0530

    installed react-router-dom and edited app js, added basic routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,25 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Home Page</div>
+  },
+  {
+    path: '/products/:id',
+    element: <div>Category</div>
+  },  
+  {
+    path: '/product/:id',
+    element: <div>Product</div>
+  }
+]);
+
 function App() {
-  return <div>Hello World!</div>;
+  return <div><RouterProvider router={router} /></div>;
 }
 
 export default App;
