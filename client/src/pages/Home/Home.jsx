b2063ea44d36b9250963a2d2be72db64634424fa
mhp <mhperera1995@gmail.com>
2023-10-15 23:01:15 +0530

    Added Contact component to home page

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Slider from './../../components/Slider/Slider';
 import Categories from './../../components/Categories/Categories';
 import FeaturedProducts from '../../components/FeaturedProducts/FeaturedProducts';
+import Contact from '../../components/Contact/Contact';
 import "./Home.scss";
 
 const Home = () => {
@@ -11,6 +12,7 @@ const Home = () => {
       <FeaturedProducts type='featured' />
       <Categories />
       <FeaturedProducts type='trending' />
+      <Contact />
     </div>
   )
 }
