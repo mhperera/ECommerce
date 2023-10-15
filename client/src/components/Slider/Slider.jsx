452a0873805178c5d4b19546f5d93d95bc029576
mhp <mhperera1995@gmail.com>
2023-10-15 19:14:42 +0530

    Change Slider Images

--- client/src/components/Slider/Slider.jsx
+++ client/src/components/Slider/Slider.jsx
@@ -4,11 +4,11 @@ import ArrowBackIosNewSharpIcon from '@mui/icons-material/ArrowBackIosNewSharp';
 import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
 
 const sliderImageList = [
+    "https://images.pexels.com/photos/3605013/pexels-photo-3605013.jpeg?auto=compress&cs=tinysrgb&w=1600&h=1000&dpr=1",
     "https://images.pexels.com/photos/2229490/pexels-photo-2229490.jpeg?auto=compress&cs=tinysrgb&w=1600&h=1000&dpr=1",
     "https://images.pexels.com/photos/298864/pexels-photo-298864.jpeg?auto=compress&cs=tinysrgb&w=1600&h=1000&dpr=1",
     "https://images.pexels.com/photos/70845/girl-model-pretty-portrait-70845.jpeg?auto=compress&cs=tinysrgb&w=1600&h=1000&dpr=1",
-    "https://images.pexels.com/photos/1300550/pexels-photo-1300550.jpeg?auto=compress&cs=tinysrgb&w=1600&h=1000&dpr=1",
-    "https://images.pexels.com/photos/179909/pexels-photo-179909.jpeg?auto=compress&cs=tinysrgb&w=1600&h=1000&dpr=1"
+    "https://images.pexels.com/photos/2419540/pexels-photo-2419540.jpeg?auto=compress&cs=tinysrgb&w=1600&h=1000&dpr=1",
 ];
 
 const Slider = () => {
