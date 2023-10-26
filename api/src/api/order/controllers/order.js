79b894dee7b4ae1ace452f2406674bd4325f4d87
mhp <mhperera1995@gmail.com>
2023-10-26 23:23:39 +0530

    order collection

---
+++ api/src/api/order/controllers/order.js
@@ -0,0 +1,12 @@
+// This is your test secret API key.
+const stripe = require('stripe')(process.env.STRIPE_KEY);
+
+'use strict';
+
+/**
+ * order controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::order.order');
